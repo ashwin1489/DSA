6f872452103fbbbede98afaf65889272188d412c
ashwin1489 <ashw5325@gmail.com>
2025-07-02 03:27:55 +0530

    Create Top view of tree.java

---
+++ Top view of tree.java
@@ -0,0 +1,68 @@
+import java.util.*;
+
+public class BinaryTreesB {
+    static class Node {
+        int data;
+        Node left, right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = this.right = null;
+        }
+    }
+
+    static class Info {
+        Node node;
+        int hd;
+
+        Info(Node node, int hd) {
+            this.node = node;
+            this.hd = hd;
+        }
+    }
+
+    public static void topview(Node root) {
+        if (root == null) return;
+
+        Queue<Info> q = new LinkedList<>();
+        HashMap<Integer, Node> map = new HashMap<>();
+
+        int min = 0, max = 0;
+        q.add(new Info(root, 0));
+
+        while (!q.isEmpty()) {
+            Info curr = q.remove();
+
+            if (!map.containsKey(curr.hd)) {
+                map.put(curr.hd, curr.node);
+            }
+
+            if (curr.node.left != null) {
+                q.add(new Info(curr.node.left, curr.hd - 1));
+                min = Math.min(min, curr.hd - 1);
+            }
+
+            if (curr.node.right != null) {
+                q.add(new Info(curr.node.right, curr.hd + 1));
+                max = Math.max(max, curr.hd + 1);
+            }
+        }
+
+        for (int i = min; i <= max; i++) {
+            System.out.print(map.get(i).data + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        topview(root);  // Just call the function; no need to print it
+    }
+}
